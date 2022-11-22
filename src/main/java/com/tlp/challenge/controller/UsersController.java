732be78e572c43e57c107cd89524b967c93273ae
Mario Altamura <mario.altamura@assioma.net>
2022-11-22 18:37:00 +0100

    Create empty UsersController.java on path /users

---
+++ src/main/java/com/tlp/challenge/controller/UsersController.java
@@ -0,0 +1,9 @@
+package com.tlp.challenge.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UsersController {
+}
