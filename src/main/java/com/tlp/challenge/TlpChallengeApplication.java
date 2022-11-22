45439e00c05f2cc26f7c4c50721c835e5ca3a1e0
Mario Altamura <mario.altamura@assioma.net>
2022-11-22 15:50:49 +0100

    Init project with spring initiliazer. Include the following additional dependencies: - spring web - lombok - jpa - H2 - spring validation

---
+++ src/main/java/com/tlp/challenge/TlpChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tlp.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlpChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlpChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlp/challenge/TlpChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlp.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlpChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
