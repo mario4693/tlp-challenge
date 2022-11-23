b678ce73faf8652fdd60db166eb7cf1c3d2fa81e
Mario Altamura <mario.altamura@assioma.net>
2022-11-23 15:28:40 +0100

    Add CustomerController test class

---
+++ src/test/java/com/tlp/challenge/controller/CustomerControllerTest.java
@@ -0,0 +1,10 @@
+package com.tlp.challenge.controller;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerControllerTest {
+
+    @Test
+    void createUser_shouldReturn201AndANewUser(){
+    }
+}
