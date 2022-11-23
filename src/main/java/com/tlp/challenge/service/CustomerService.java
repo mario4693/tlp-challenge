867118cf74878c15cc6281506097d07aab5d5c7c
Mario Altamura <mario.altamura@assioma.net>
2022-11-23 18:01:39 +0100

    Add createUser in CustomerService

---
+++ src/main/java/com/tlp/challenge/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.tlp.challenge.service;
+
+import com.tlp.challenge.dto.CustomerDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public CustomerDTO createUser() {
+        return null;
+    }
+}
