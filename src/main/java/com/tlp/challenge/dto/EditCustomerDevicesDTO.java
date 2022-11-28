1b033065c20b801567d1e4a6549b973a2e188fcb
Mario Altamura <mario.altamura@assioma.net>
2022-11-28 16:33:51 +0100

    Add useful dtos for add devices to customer

---
+++ src/main/java/com/tlp/challenge/dto/EditCustomerDevicesDTO.java
@@ -0,0 +1,7 @@
+package com.tlp.challenge.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public record EditCustomerDevicesDTO(@NotNull Long customerId, @NotNull(message = "should provide at least one new device") @Size(min = 1, max = 2) List<NewDeviceDTO> devices) {}
---
+++ src/main/java/com/tlp/challenge/dto/NewDeviceDTO.java
@@ -0,0 +1,5 @@
+package com.tlp.challenge.dto;
+
+import com.tlp.challenge.entity.Device.DeviceState;
+
+public record NewDeviceDTO(DeviceState state) {}
