4ed8ba6688064a34c7b057f9e9e5372af945a549
Mario Altamura <mario.altamura@assioma.net>
2022-11-29 19:09:21 +0100

    Add findByCustomerId method

--- src/main/java/com/tlp/challenge/repository/DeviceRepository.java
+++ src/main/java/com/tlp/challenge/repository/DeviceRepository.java
@@ -3,6 +3,10 @@ package com.tlp.challenge.repository;
 import com.tlp.challenge.entity.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface DeviceRepository extends JpaRepository<Device, UUID> {}
+public interface DeviceRepository extends JpaRepository<Device, UUID> {
+    Optional<List<Device>> findByCustomerId(Long id);
+}
