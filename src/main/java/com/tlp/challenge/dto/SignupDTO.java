917729e74e0d4faae5388eb121b0784902c95bbc
Mario Altamura <mario.altamura@assioma.net>
2022-12-02 09:52:07 +0100

    Add SignupDTO.java fields validation

--- src/main/java/com/tlp/challenge/dto/SignupDTO.java
+++ src/main/java/com/tlp/challenge/dto/SignupDTO.java
@@ -1,5 +1,6 @@
 package com.tlp.challenge.dto;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-public record SignupDTO(String name, String surname, String fiscalCode, String address, @Size(max = 2) DeviceDTO... devices) {}
+public record SignupDTO(@NotEmpty String name,@NotEmpty String surname,@NotEmpty String fiscalCode,@NotEmpty String address, @Size(max = 2) DeviceDTO... devices) {}
