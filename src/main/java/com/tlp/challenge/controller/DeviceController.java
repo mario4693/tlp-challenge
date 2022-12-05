d50edad10ed3ee6cf992e8fe7e2030cc88d5c48f
Mario Altamura <mario.altamura@assioma.net>
2022-12-05 10:59:08 +0100

    Add some docs to DeviceController.java

--- src/main/java/com/tlp/challenge/controller/DeviceController.java
+++ src/main/java/com/tlp/challenge/controller/DeviceController.java
@@ -6,6 +6,9 @@ import com.tlp.challenge.dto.NewDeviceDTO;
 import com.tlp.challenge.exception.CustomerDevicesFullException;
 import com.tlp.challenge.exception.CustomerNotFoundException;
 import com.tlp.challenge.service.DeviceService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +27,11 @@ public class DeviceController {
         this.deviceService = deviceService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "New created device"),
+            @ApiResponse(responseCode = "404", description = "Customer to pair with new device not found", content = @Content),
+            @ApiResponse(responseCode = "422", description = "Server can't add also this new device", content = @Content)
+    })
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<DeviceDTO> createDevice(@Valid @RequestBody NewDeviceDTO newDeviceDTO) {
         try {
@@ -35,16 +43,29 @@ public class DeviceController {
         }
     }
 
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Device specified is present"),
+            @ApiResponse(responseCode = "404", description = "Device not found", content = @Content),
+    })
     @GetMapping(value = "{uuid}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> isDevicePresent(@PathVariable UUID uuid) {
         return deviceService.isDevicePresent(uuid) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Device specified has been deleted"),
+            @ApiResponse(responseCode = "404", description = "Device not found", content = @Content),
+    })
     @DeleteMapping(value = "{uuid}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> deleteDevice(@PathVariable UUID uuid) {
         return deviceService.deleteDeviceById(uuid) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "New edited device"),
+            @ApiResponse(responseCode = "404", description = "Device to updated not found", content = @Content),
+    })
     @PatchMapping(value = "{uuid}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<DeviceDTO> editDeviceState(@PathVariable UUID uuid, @Valid @RequestBody EditDeviceStateDTO editDeviceStateDTO) {
         var optionalDeviceDTO = deviceService.editDeviceState(uuid, editDeviceStateDTO.state());
